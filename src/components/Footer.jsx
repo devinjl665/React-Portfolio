942b58ce7246b1e4c5f88ff5abf8e27f73e7042a
elroy <devinjl665@gmail.com>
2024-03-10 20:02:00 -0500

    Added code to footer and header.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+
+    return (
+        <footer>
+            <section class="center"> 
+                <ul>
+                    <li><a href="" target="_blank">Github</a></li>
+                    <li><a href="" target="_blank">Linkedin</a></li>
+                    <li><a href="" target="_blank">Stack_Overflow</a></li>
+                </ul>
+            </section>
+        </footer>
+    );
+
+
+}
+
+export default Footer;
