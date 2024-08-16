edd9d79b4f177d4026366becc4c2a713a813309e
elroy <devinjl665@gmail.com>
2024-08-15 19:00:07 -0500

    Removed template to start adding my own features

--- React-Portfolio/src/main.jsx
+++ React-Portfolio/src/main.jsx
@@ -1,7 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+import "@fontsource/roboto";
+import "@fontsource/outfit";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
