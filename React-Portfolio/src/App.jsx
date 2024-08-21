936538021e31f4e952b5022dda5c3b2a64469528
elroy <devinjl665@gmail.com>
2024-08-20 21:38:05 -0500

    Added to hero section

--- React-Portfolio/src/App.jsx
+++ React-Portfolio/src/App.jsx
@@ -1,10 +1,13 @@
 import styles from "./App.module.css";
-import { Navbar} from "./components/navbar/navbar";
+import { Navbar } from "./components/navbar/navbar";
+import { Hero } from "./components/hero/hero";
 
 function App() {
   return (
     <div className={styles.App}> 
         <Navbar />
+        <Hero />
+        
     </div>
   );
 }
---
+++ React-Portfolio/src/components/hero/hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./hero.module.css";
+
+export const Hero = () => {
+    return <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Devin Little</h1>
+            <p className={styles.description}>I'm a self-driven junior full stack developer that easily adapts to different challenges thrown their way. Specializing in HTML5, CSS, JavaScript, MySQL, and MongoDB. Looking to be a team member for a company that wants to lead in innovation in their respective field.
+            </p>
+            <a href="mailto:devinjl665@gmail.com" className={styles.contactBtn}>Contact Me!</a>
+        </div>
+        <img src={getImageUrl("hero/Devin.png")} alt="Image of Devin Little" className={styles.heroImg}/>
+            <div className={styles.topBlur} />
+            <div className={styles.bottomBlur} />
+    </section>;
+
+};
