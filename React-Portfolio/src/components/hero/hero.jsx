bc92c88846bfa42d861d0771c7f401fc4cca7fb6
elroy <devinjl665@gmail.com>
2024-09-08 03:45:23 -0500

    Added projects section

--- React-Portfolio/src/components/hero/hero.jsx
+++ React-Portfolio/src/components/hero/hero.jsx
@@ -9,8 +9,8 @@ export const Hero = () => {
                 <h1 className={styles.title}>Devin Little</h1>
                 <p className={styles.description}>I'm a self-driven junior full stack developer that easily adapts to different challenges thrown their way. Specializing in HTML5, CSS, JavaScript, MySQL, and MongoDB. Looking to be a team member for a company that wants to lead in innovation in their respective field.
                 </p>
-                <a href="mailto:devinjl665@gmail.com" className={styles.contactBtn}>Contact Me!</a>
-                <a href="" className={styles.resumeBtn}>Resume</a>
+                <a href="mailto:devinjl665@gmail.com" className={styles.btn}>Contact Me</a>
+                <a href="" className={styles.btn}>Resume</a>
             </div>
             <img src={getImageUrl("hero/Devin.png")} alt="Image of Devin Little" className={styles.heroImg}/>
                 <div className={styles.topBlur} />
--- React-Portfolio/src/components/projects/projectsCard.jsx
+++ React-Portfolio/src/components/projects/projectsCard.jsx
@@ -7,12 +7,16 @@ import styles from "./projectsCard.module.css";
 export const ProjectsCard = ({project : {title, imageSrc, description, skills, demo, source}}) => {
     return (
         <div className={styles.container}>
-            <img src={getImageUrl(imageSrc)} alt={`Image of ${title}`} className={styles.image}/>
+            <img src={getImageUrl("projects/projectlogo.png")} alt={`Image of ${title}`} className={styles.image}/>
             <h3 className={styles.title}>{title}</h3>
             <p className={styles.description}>{description}</p>
             <ul className={styles.skills}>
                 {skills.map((skill, id) => {
-                    <li key={id} className={styles.skill}></li>
+                    return (
+                        <li key={id} className={styles.skill}>
+                            {skill}
+                        </li>
+                    )  
                 })}
             </ul>
             <div className={styles.links}>
