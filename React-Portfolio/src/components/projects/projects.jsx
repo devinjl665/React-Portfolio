42e65a13825e3da1de992013dacf0ff16a0505b2
elroy <devinjl665@gmail.com>
2024-09-08 02:21:34 -0500

    Started projects section

---
+++ React-Portfolio/src/components/projects/projects.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import projects from "../../data/projects.json";
+
+import { ProjectsCard } from "./projectsCard";
+
+import styles from "./projects.module.css";
+
+
+export const Projects = () => {
+    return (
+        <section className={styles.container}>
+            <h2 className={styles.title}>Projects</h2>
+            <div className={styles.projects}>
+                {
+                    projects.map((project, id) => {
+                        return (
+                            <ProjectsCard key={id} project={project} />
+                        
+                    )})
+                }
+            </div>
+        </section>
+    );
+};
