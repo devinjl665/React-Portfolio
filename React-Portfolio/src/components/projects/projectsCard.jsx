b577d598db1a1f3c0f082ecff2916f277b3025c8
elroy <devinjl665@gmail.com>
2024-11-13 15:22:16 -0600

    Added images and updated project cards

--- React-Portfolio/src/components/projects/projectsCard.jsx
+++ React-Portfolio/src/components/projects/projectsCard.jsx
@@ -7,7 +7,7 @@ import styles from "./projectsCard.module.css";
 export const ProjectsCard = ({project : {title, imageSrc, description, skills, demo, source}}) => {
     return (
         <div className={styles.container}>
-            <img src={getImageUrl("projects/projectlogo.png")} alt={`Image of ${title}`} className={styles.image}/>
+            <img src={getImageUrl(imageSrc)} alt={`Image of ${title}`} className={styles.image}/>
             <h3 className={styles.title}>{title}</h3>
             <p className={styles.description}>{description}</p>
             <ul className={styles.skills}>
